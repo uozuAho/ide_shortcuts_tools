6d9927bb13a66f58debf4db540dacb0fa6b85872
Warwick Stone <Warwick.Stone@myob.com>
2020-11-23 19:41:18 +1100

    extract number adder from cannon firer

--- StarCannon/refactored/CannonFirer.cs
+++ StarCannon/refactored/CannonFirer.cs
@@ -5,6 +5,19 @@ using StarCannon.refactored.CannonBits;
 
 namespace StarCannon.refactored
 {
+    public class NumberAdder
+    {
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int AddNumbers(IEnumerable<int> numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+
     public class CannonFirer
     {
         public void FireTheCannon()
@@ -16,21 +29,11 @@ namespace StarCannon.refactored
 
         private static void ChargeTheCapacitor()
         {
-            var theNumberes = AddNumbers(1, 2);
-            var theOtherNumbers = AddNumbers(new List<int> {1, 2, 3, 4});
+            var theNumberes = NumberAdder.AddNumbers(1, 2);
+            var theOtherNumbers = NumberAdder.AddNumbers(new List<int> {1, 2, 3, 4});
 
             const int size = 3;
             var capacitor = new FluxCapacitor(size);
-        }
-
-        private static int AddNumbers(int a, int b)
-        {
-            return a + b;
-        }
-
-        private static int AddNumbers(IEnumerable<int> numbers)
-        {
-            return numbers.Sum();
         }
     }
 }
